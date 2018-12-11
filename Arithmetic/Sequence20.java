006ae3c5d56c47624a8dccb29fe50b666d525a1e
namgiho96 <namgiho96@gmail.com>
2018-12-11 16:43:43 +0900

    18.12.11 16:43

---
+++ Arithmetic/Sequence20.java
@@ -0,0 +1,16 @@
+package Arithmetic;
+/** 
+ * 등차 수열은 두 항의 차이는 이 수열
+ * 공차 : 공통적으로 나타나는 차이
+ *2
+ * **/
+
+public class Sequence20 {
+	public static void main(String[] args) {
+		int A = 2;
+		int S = A;
+		int D = 6;
+		int N = 2;
+		int AN = 0;
+	}
+}
